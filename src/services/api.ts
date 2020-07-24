701b78abb45b19ac9e357c05cf81f0b4089e46af
Itamar1986 <itamar_minnow@hotmail.com>
2020-07-23 22:52:48 -0300

    Finalizando desafio 08

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,3 @@
-/*import axios from 'axios';
-
-const api = axios.create({
-    baseURL: 'http://192.168.0.105:3333',
-});
-
-export default api;*/
-
 import axios from 'axios';
 
 const api = axios.create({
